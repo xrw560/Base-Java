f5f5cf6069038a8145cae65744b1c1c7d1091967
zhouning <ncutits@163.com>
2017-10-16 12:04:43 +0800

    格式化

--- src/main/java/cn/ncut/java/base/mythread/thread/lock/MyLockTest.java
+++ src/main/java/cn/ncut/java/base/mythread/thread/lock/MyLockTest.java
@@ -5,47 +5,52 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class MyLockTest {
-	private static ArrayList<Integer> arrayList = new ArrayList<Integer>();
-	static Lock lock = new ReentrantLock(); // 注意这个地方
-	public static <E> void main(String[] args) {
-		new Thread() {
-			public void run() {
-				Thread thread = Thread.currentThread();
-				
-				lock.lock();
-				try {
-					System.out.println(thread.getName() + "得到了锁");
-					for (int i = 0; i < 5; i++) {
-						arrayList.add(i);
-					}
-				} catch (Exception e) {
-					// TODO: handle exception
-				} finally {
-					System.out.println(thread.getName() + "释放了锁");
-					lock.unlock();
-				}
+    private static ArrayList<Integer> arrayList = new ArrayList<Integer>();
+    static Lock lock = new ReentrantLock(); // 注意这个地方
 
-			};
-		}.start();
-		
-		new Thread() {
-			public void run() {
-				Thread thread = Thread.currentThread();
-				lock.lock();
-				try {
-					System.out.println(thread.getName() + "得到了锁");
-					for (int i = 0; i < 5; i++) {
-						arrayList.add(i);
-					}
-				} catch (Exception e) {
-					// TODO: handle exception
-				} finally {
-					System.out.println(thread.getName() + "释放了锁");
-					lock.unlock();
-				}
+    public static <E> void main(String[] args) {
+        new Thread() {
+            public void run() {
+                Thread thread = Thread.currentThread();
 
-			};
-		}.start();
-	}
+                lock.lock();//获取锁
+                try {
+                    System.out.println(thread.getName() + "得到了锁");
+                    for (int i = 0; i < 5; i++) {
+                        arrayList.add(i);
+                    }
+                } catch (Exception e) {
+                    // TODO: handle exception
+                } finally {
+                    System.out.println(thread.getName() + "释放了锁");
+                    lock.unlock();
+                }
+
+            }
+
+            ;
+        }.start();
+
+        new Thread() {
+            public void run() {
+                Thread thread = Thread.currentThread();
+                lock.lock();
+                try {
+                    System.out.println(thread.getName() + "得到了锁");
+                    for (int i = 0; i < 5; i++) {
+                        arrayList.add(i);
+                    }
+                } catch (Exception e) {
+                    // TODO: handle exception
+                } finally {
+                    System.out.println(thread.getName() + "释放了锁");
+                    lock.unlock();
+                }
+
+            }
+
+            ;
+        }.start();
+    }
 
 }
