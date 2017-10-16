d015e14102116a511a79be2260c4f45627c2cafa
zhouning <ncutits@163.com>
2017-10-16 18:18:31 +0800

    反射

--- src/main/java/cn/ncut/java/base/reflect/Person.java
+++ src/main/java/cn/ncut/java/base/reflect/Person.java
@@ -2,56 +2,58 @@ package cn.ncut.java.base.reflect;
 
 import java.io.Serializable;
 
-public class Person implements Serializable,TestInterface{
-	private Long id;
-	public String name;
+public class Person implements Serializable, TestInterface {
+    private Long id;
+    public String name;
 
-	public Person() {
-		this.id = 100L;
-		this.name = "afsdfasd";
-	}
+    public Person() {
+        this.id = 100L;
+        this.name = "afsdfasd";
+    }
 
-	public Person(Long id, String name) {
+    public Person(Long id, String name) {
 //		super();
-		this.id = id;
-		this.name = name;
-	}
-	
-	
-	public Person(Long id) {
-		super();
-		this.id = id;
-	}
-	@SuppressWarnings("unused")
-	private Person(String name) {
-		super();
-		this.name = name+"=======";
-	}
+        this.id = id;
+        this.name = name;
+    }
 
-	public Long getId() {
-		return id;
-	}
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public Person(Long id) {
+        super();
+        this.id = id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    @SuppressWarnings("unused")
+    private Person(String name) {
+        super();
+        this.name = name + "=======";
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public String toString() {
-		return "Person [id=" + id + ", name=" + name + "]";
-	}
-	private String getSomeThing() {
-		return "sdsadasdsasd";
-	}
-	
-	private void testPrivate(){
-		System.out.println("this is a private method");
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        return "Person [id=" + id + ", name=" + name + "]";
+    }
+
+    private String getSomeThing() {
+        return "sdsadasdsasd";
+    }
+
+    private void testPrivate() {
+        System.out.println("this is a private method");
+    }
 }
